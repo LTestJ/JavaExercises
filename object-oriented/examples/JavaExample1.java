a57adf9fa17af9a608d9aa79d38e099d5f1de7ba
Vimal Earnest <v_earnest@yahoo.com>
2018-10-25 21:57:05 -0700

    added example problems

---
+++ object-oriented/examples/JavaExample1.java
@@ -0,0 +1,12 @@
+class Person {
+};
+
+public class JavaExample1 {
+  public static void main(String[] args){
+    
+    //Creating person object
+    Person p = new Person();
+ 
+    System.out.println("Created person object");    
+  }
+}
