4398f3d9feba987c72b8b6ef3ad599151a859f03
Jose Devassy <jose123go@gmail.com>
2018-11-10 19:25:57 -0800

    adding Ch5,6,7

---
+++ object-oriented/examples/Chapter6/Haiku.java
@@ -0,0 +1,9 @@
+public class Haiku
+{
+  public static void main ( String[] args )
+  {
+    System.out.println("On a withered branch");
+    System.out.println("A crow has just alighted:");
+    System.out.println("Nightfall in autumn.");
+  }
+}
