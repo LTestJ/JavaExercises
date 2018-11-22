560a50789c5a7f10f6632dc260e4aff2d3a6ad37
Jose Devassy <jose123go@gmail.com>
2018-11-21 21:26:48 -0800

    adding files of Chapter11

---
+++ object-oriented/examples/Chapter11/ImportDemo3.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class ImportDemo3
+{
+  public static void main ( String[] args )
+  {
+    Scanner scan ;
+
+    scan = new Scanner( System.in );
+
+    // do something with the Scanner object (see next chapter)
+  }
+}
