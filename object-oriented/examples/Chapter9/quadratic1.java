8ff21cb5255cb1ab1148841d2bc194d17637d0ef
Jose Devassy <jose123go@gmail.com>
2018-11-28 21:05:38 -0800

    quadratic file

--- object-oriented/examples/Chapter9/quadratic1.java
+++ object-oriented/examples/Chapter9/quadratic1.java
@@ -1,11 +1,11 @@
-public class quadratic
+public class quadratic1
 {
   public static void main ( String[] args )
   {
 
     double X = 3;
-
-    System.out.println("Total Value: " + 4*X*X (-8*X) +4  );
+    double result = 4 * X * X -8*X + 4;
+    System.out.println("Total Value: " + result);
 
   }
 }
