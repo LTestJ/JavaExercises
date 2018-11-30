08c6a8fcc83d4b1d881498f6d137d94c79523ce3
Jose Devassy <jose123go@gmail.com>
2018-11-29 21:04:32 -0800

    adding files for Chapter12

--- object-oriented/examples/Chapter12/CorrectChange.java
+++ object-oriented/examples/Chapter12/CorrectChange.java
@@ -5,21 +5,26 @@ public class CorrectChange
   public static void main (String[] args)
   {
     Scanner scan = new Scanner( System.in );
-    int change, dollar, quarter, dime, nickel, cent;
-//    dollar  = 100;
-//    quarter = 25;
-//    dime = 10;
-//    nickel = 5;
-//    cent = 1;
+    int change, dollar, quarter, dime, nickel, cent, quotient1, quotient2, quotient3, quotient4;
 
     System.out.println("Enter the change: ");
     change = scan.nextInt();
 
-    change / 100 = dollar;
-    change % 100 = cent;
+    dollar = change / 100;
+    quotient1 = change % 100;
 
-    System.out.println("That is " + dollar + " Dollars and  " + cent + 
-"Cents" );
+    quarter = quotient1 / 25;
+    quotient2 = quotient1 % 25;
+
+    dime = quotient2 /10;
+    quotient3 = quotient2 % 10;
+
+    nickel = quotient3 / 5;
+    quotient4 = quotient3 % 5;
+
+
+    System.out.println( change + " is " + dollar + " Dollars and " + quarter + 
+" Quarter " + dime + " Dime " + nickel + " Nickel " + quotient4 + " pennies " );
   }
 }
 
--- object-oriented/examples/Chapter12/OhmsLaw.java
+++ object-oriented/examples/Chapter12/OhmsLaw.java
@@ -4,16 +4,16 @@ public class OhmsLaw
   public static void main (String[] args)
   {
     Scanner scan = new Scanner( System.in );
-    int Voltage, Current, Resistance;
+    int Voltage, Resistance;
+    double Current;
 
     System.out.println("Enter the Voltage: ");
     Voltage = scan.nextInt();
-    System.out.println("Enter the Current: ");
+
+    System.out.println("Enter the Resistance: ");
     Resistance = scan.nextInt();
 
-//    Current = Voltage / Resistance;
     Current = ( Voltage + 0.0 ) / Resistance;
-
     System.out.println("The Current is " + Current );
   }
 }
