fb3a5a48112bc9a69cb712a2996fb09fa68a23d4
Jose Devassy <jose123go@gmail.com>
2018-12-03 15:51:05 -0800

    correction files Ch9

--- object-oriented/examples/Chapter9/quadratic1.java
+++ object-oriented/examples/Chapter9/quadratic1.java
@@ -3,8 +3,8 @@ public class quadratic1
   public static void main ( String[] args )
   {
 
-    double X = 3;
-    double result = 4 * X * X -8*X + 4;
+    double X = 2;
+    double result = (3 * X * X) - (8*X) + 4;
     System.out.println("Total Value: " + result);
 
   }
---
+++ object-oriented/examples/Chapter9/quadratic2.java
@@ -0,0 +1,39 @@
+
+
+public class quadratic2
+{
+  public static void main ( String[] args )
+  {
+    double X1 = 2;
+    double value1 = (3 * X1 * X1 ) -(8 * X1) + 4;
+    System.out.println("Value1 is: " + value1);
+
+
+    double X2 = 3;
+    double value2 = (3 * X2 * X2 ) -(8 * X2) + 4;
+    System.out.println("Value2 is: " + value2);
+
+
+    double X3 = 4;
+    double value3 = (3 * X3 * X3 ) -(8 * X3) + 4;
+    System.out.println("Value3 is: " + value3);
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
