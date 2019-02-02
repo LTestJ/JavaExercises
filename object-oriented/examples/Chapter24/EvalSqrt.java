647419691f8e5bafa00102137fd7b95588bea7ca
Vimal Earnest <v_earnest@yahoo.com>
2019-02-02 01:03:51 +0000

    chap24

---
+++ object-oriented/examples/Chapter24/EvalSqrt.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EvalSqrt
+{
+  public static void main (String[] args )
+  {
+    Scanner scan = new Scanner( System.in );
+    double x;
+
+    System.out.print("Enter a value for x or -1 to exit: ")  ;
+    x = scan.nextDouble();
+
+    for (    ; x >= 0.0 ;   )
+    {
+      System.out.println( "Square root of " + x + " is " + Math.sqrt( x ) );
+      System.out.print("Enter a value for x or -1 to exit: ")  ;
+      x =  scan.nextDouble();
+    }
+  }
+}
