ae7e55069bb6210aa409a0cc71a2b0f8691d639d
Vimal Earnest <v_earnest@yahoo.com>
2019-02-16 19:48:40 +0000

    fix 22

--- object-oriented/examples/Chapter22/AddUpTwo.java
+++ object-oriented/examples/Chapter22/AddUpTwo.java
@@ -2,52 +2,50 @@ import java.util.*;
 
 // Add up integers entered by the user.
 // After the last integer, the user enters a 0.
-//
 public class AddUpTwo
 {
-  public static void main (String[] args )
+  public static void main (String[] args)
   {
-    Scanner scan = new Scanner( System.in );
+    Scanner scan = new Scanner(System.in);
     String inputData;
-    String suffix;
+    String suffix = "st";
     int value;             // integer entered by the user
     int count = 0;         // how many integers added so far
-    int sum   = 0;         // initialize the sum
-    // get the first value
-    System.out.print( "Enter first integer (enter 0 to quit): " );
-    value = scan.nextInt();
+    int sum = 0;         // initialize the sum
 
     while (true)
     {
-      if (value == 0)
+      System.out.println("Enter the " + (count+1) + suffix + " number (enter 0 to quit):");
+      value = scan.nextInt();
+
+      if (value == 0) //Should we exit ?
       {
-        System.out.println("No integers are added\nBye");
+        if (sum == 0) {
+          System.out.println("No integers are added.");
+        }
+        else {
+          System.out.println("Sum of the integers: " + sum);
+        }
+        System.out.println("Bye\n");
         break;
       }
-
-      else
+      else // no, we need to process it
       {
         //add value to sum
         sum   = sum + value;     // add current value to the sum
         count = count + 1;       // one more integer added
         // prompt for the next value
-        if ( count+1  == 2  )
+        if (count+1 == 2)
         {
           suffix = "nd";
         }
         else
         {
-          if ( count+1 == 3  )
+          if (count+1 == 3)
             suffix = "rd";
           else
             suffix = "th";
         }
-        System.out.print( "Enter the next " + (count+1) + suffix +
-                          " integer (enter 0 to quit): " );
-        //get the next value from the user
-        value = scan.nextInt();
-
-        System.out.println( "Sum of the integers: " + sum );
       }
     }
   }
--- object-oriented/examples/Chapter22/InRangeAdder.java
+++ object-oriented/examples/Chapter22/InRangeAdder.java
@@ -8,36 +8,31 @@ public class InRangeAdder
     int value, value1, value2;
     int sumIn = 0;
     int sumOut = 0;
-    System.out.print( "Low end of range: " );
+    System.out.print("Low end of range: ");
     value1 = scan.nextInt();
     System.out.print("High end of range: ");
     value2 = scan.nextInt();
-    System.out.println("Enter data: ");
-    value = scan.nextInt();
 
-    while (true)
+    while(true)
     {
+      System.out.println("Enter data: ");
+      value = scan.nextInt();
+
       if (value == 0)
       {
         System.out.println("Bye");
         break;
       }
-      else if  ( value >= value1 && value <= value2 )
+      else if ( value >= value1 && value <= value2)
       {
         sumIn = sumIn + value;
-//        System.out.println( "Enter your data: " );
-        value = scan.nextInt();
       }
       else
       {
         sumOut = sumOut + value;
-//        System.out.println("Enter yours data: ");
-        value = scan.nextInt();
       }
-//        System.out.println("Enter your data: ");
-//        value = scan.nextInt();
     }
-    System.out.println( "Sum inside Range: " + sumIn );
-    System.out.println("Sum outside Range: " + sumOut );
+    System.out.println( "Sum inside Range: " + sumIn);
+    System.out.println("Sum outside Range: " + sumOut);
   }
 }
