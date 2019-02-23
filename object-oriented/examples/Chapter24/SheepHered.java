dc0d27f8a4da539acaab654a68c3313350368255
Vimal Earnest <v_earnest@yahoo.com>
2019-02-23 05:23:43 +0000

    EX1

---
+++ object-oriented/examples/Chapter24/SheepHered.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.text.*;
+
+public class SheepHered
+{
+  public static void main (String[] args )
+  {
+    DecimalFormat numform = new DecimalFormat();
+    double N = 20;
+    double t = 0;
+
+    for ( ; t < 25; t++)
+    {
+
+       N = 220 / (1 + 10 * Math.pow(0.83, t));
+
+       if (N > 90)
+       {
+         break;
+       }
+
+       System.out.println("Years: " + t + "\tSheeps: " + numform.format(N));
+    }
+  }
+}
