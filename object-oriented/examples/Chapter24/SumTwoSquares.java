f8abc14dbb85ede1f32f8a719049daf7aa58e85e
Vimal Earnest <v_earnest@yahoo.com>
2019-02-26 00:35:56 +0000

    n

--- object-oriented/examples/Chapter24/SumTwoSquares.java
+++ object-oriented/examples/Chapter24/SumTwoSquares.java
@@ -7,25 +7,30 @@ public class SumTwoSquares
     Scanner scan = new Scanner(System.in);
     System.out.print("Enter the integer: ");
     int n = scan.nextInt();
-    int a = 0;
-    int b = 0;
+//    int a = 0;
+//    int b = 0;
 
-    for (a = 1; a <= (n*n); a++)
+    for (int a = 1; a <= (n*n); a++)
     {
-      for (b = 1; b <= (n*n); b++)
+      for (int b = 1; b <= (n*n); b++)
       {
         if (a * a + b * b == n)
         {
           System.out.println(n + " is sum of squares of two integers " + a + " and " + b);
           break;
         }
+        else
+        {
+          System.out.println("not sum");
+          break;
+        }
       }
-      if (a * a + b * b != n)
+/*      if (a * a + b * b != n)
       {
         System.out.println(" not sum of squares");
         break;
       }
-    }
+*/    }
 /*    if ( a * a + b * b != n)
     {
       System.out.println("Not sum of squares of two integers");
