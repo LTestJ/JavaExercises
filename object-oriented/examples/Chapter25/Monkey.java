70d1b56fd4230cfdd7ff085db3fc771cf3aa3c2f
Vimal Earnest <v_earnest@yahoo.com>
2019-03-01 05:18:06 +0000

    Ch25_4

--- object-oriented/examples/Chapter25/Monkey.java
+++ object-oriented/examples/Chapter25/Monkey.java
@@ -12,18 +12,17 @@ public class Monkey
     String[] monk = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
 
     for (int i = (n-1); i <= monk.length; i--)
-//    for (int i = 1; i <= n; i++)
     {
-        System.out.println("\n" + monk[i] + " little monkeys swinging in a tree\nTeasing Mr Crocodile" + "\"you can't catch me\"");
-        System.out.println("Along came the crocodile as quiet as can be\nand SNAP!");
+       System.out.println("\n" + monk[i] + " little monkeys swinging in a tree\nTeasing Mr Crocodile" + "\"you can't catch me\"");
+       System.out.println("Along came the crocodile as quiet as can be\nand SNAP!");
 
-      if (i == 0)
-      {
-        break;
-      }
+       if (i == 0)
+       {
+         break;
+       }
     }
-    System.out.println("\nNo little monkeys swinging in a tree\nNo little monkeys swinging in a tree");
 
+    System.out.println("\nNo little monkeys swinging in a tree\nNo little monkeys swinging in a tree");
   }
 }
 
